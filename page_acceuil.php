fd92878635e60f7f7aa463fd92e60da4ca907d08
Hugo LECUYER <hlecuyer@e2r7p7.42.fr>
2014-04-05 17:15:39 +0200

    retour de la page d acceuil

--- page_acceuil.php
+++ page_acceuil.php
@@ -1,29 +1,29 @@
 <?PHP
-	echo '<!DOCTYPE>';
-	echo '<html>';
-	echo '<head>';
-	echo	'<title>Shop 42</title>';
-	echo '</head>';
-	echo '<body>';
-	echo '<h1>SHOP 42</h1>';
+	echo "<!DOCTYPE>\n";
+	echo "<html>\n";
+	echo "<head>\n";
+	echo "	<title>Shop 42</title>\n";
+	echo "</head>\n";
+	echo "<body>\n";
+	echo "	<h1>SHOP 42</h1>\n";
 	if (isset($_COOKIE["user"]))
 	{
-		echo '<p>Bonjour $_COOKIE["user"]["login"]</p>';
-		echo '<a href=modif_user.html>Modifier les information de mon compte</a>';
-	}
-	else
-	{
-		echo '<p>Bonjour cher visiteur</p>';
-		echo '<form action="connect.php" method="post">';
-				echo '<input type="text" name="login" size="72" class="text"><br />';
-				echo '<input type="password" name="passwd" size="72" class="text">';
-				echo '<input type="submit" value="Se connecter" id="button">';
-		echo '</form>';
-		echo '<a href=add_user.html>Je souhaite me creer un compte</a>';
+		echo "	<p>Bonjour" . $_COOKIE["user"]["login"] . "</p>\n";
+		echo "	<a href=modif_user.html>Modifier les information de mon compte</a>\n";
 	}
 	if (isset($_COOKIE["user"]["admin"]))
 	{
-		echo '<a href="add_product">Ajouter un produit</a>';
+		echo "	<a href=\"add_product\">Ajouter un produit</a>\n";
 	}
-	echo '</body>';
+	else
+	{
+		echo "	<p>Bonjour cher visiteur</p>\n";
+		echo "	<form action=\"connect.php\" method=\"post\">\n";
+				echo "		<input type=\"text\" name=\"login\" size=\"72\" class=\"text\"><br />\n";
+				echo "		<input type=\"password\" name=\"passwd\" size=\"72\" class=\"text\">\n";
+				echo "		<input type=\"submit\" value=\"Se connecter\" id=\"button\">\n";
+		echo "	</form>\n";
+		echo "	<a href=add_user.html>Je souhaite me creer un compte</a>\n";
+	}
+	echo "</body>\n";
 ?>
