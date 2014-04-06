bba446149e9cd653a409f61efb14c4f1e693915a
Hugo LECUYER <hlecuyer@e2r7p7.42.fr>
2014-04-06 15:24:30 +0200

    push

--- print_right_bar_bottom.php
+++ print_right_bar_bottom.php
@@ -5,14 +5,18 @@ function print_right_bar_bottom()
 	echo "<div classe=\"right_bar\">\n";
 	if (isset($_COOKIE["pannier"]))
 	{
-		foreach ($_COOKIE["pannier"] as $elem)
+		$array = unserialize($_COOKIE["pannier"]);
+		foreach ($array as $elem)
 		{
-			echo "<p class=name_of_product>" . $elem['name'] . "</p>/n";
-			echo "<p class=quantity_of_product>" . $elem['quantity'] . "</p>/n";
+			echo "<p class=name_of_product>" . $elem['name'] . "</p>\n";
+			echo "<p class=quantity_of_product>" . $elem['quantity'] . "</p>\n";
 		}
+		echo "<form align=\"center\" action=\"add_product.php\" method=\"POST\">\n";
+		echo "<input type=\"submit\" name=\"submit\" value=\"Je valide ma commande\" />\n";
+		echo "</form>";
 	}
 	else
-	echo "<p>votre pannier est vide</p>";
+	echo "<p>votre pannier est vide</p>\n";
 	echo "</div>\n";
 	echo "</body>\n";
 }
